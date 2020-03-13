2e76c83ea9118a47a634e161fb894e5ba69fdd5c
juanorro <juan.rodriguezjim@gmail.com>
2020-03-13 19:29:09 +0100

    app done

--- app.js
+++ app.js
@@ -7,3 +7,62 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+const columnRoutes = require('./routes/colums.routes');
+const cardsRoutes = require('./routes/cards.routes');
+const authRoutes = require('./routes/auth.routes');
+
+require('./configs/db.config');
+const session = require('./configs/session.config');
+const cors = require('./configs/cors.config');
+require('./configs/passport.config');
+
+const app = express();
+
+app.use(logger('env'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser()); 
+app.use(cors);
+
+app.use(session);
+app.use(passport.initialize);
+app.use(passport.session());
+
+app.use('/colums', columnRoutes);
+app.use('/cards', cardsRoutes);
+app.use('/', authRoutes);
+
+app.use((req, res, next) => {
+    res.local.session = req.user; 
+    next()
+});
+
+//404
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+//error handler
+
+app.user(function(error, req, res, next) {
+    console.error(error);
+
+    res.status(error.status || 500);
+
+    const data = {};
+
+    if(error instanceof mongoose.Error.ValidationError) {
+        res.status(400); 
+        for(field of Object.keys(error.errors)) {
+            error.errors[field] = error.errors[field].message
+        }
+        data.errors = error.errors
+    } else if (error instanceof mongoose.Error.CastError) {
+        error = createError(404, 'Resource not found')
+    }
+
+    data.message = error.message; 
+    res.json(data);
+})
+
+module.exports = app; 
